09e1f0b5d5d62109068c1424370bf5b10886edc1
Aman <fz72eaman@gmail.com>
2021-07-06 13:02:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const body = document.body;
+// body.onload = fadeOutEffect;
+
+// function fadeOutEffect() {
+//   var fadeTarget = document.getElementById("target");
+//   var fadeEffect = setInterval(function () {
+//     if (!fadeTarget.style.opacity) {
+//       fadeTarget.style.opacity = 1;
+//     }
+//     if (fadeTarget.style.opacity > 0) {
+//       fadeTarget.style.opacity -= 0.1;
+//     } else {
+//       clearInterval(fadeEffect);
+//     }
+//   }, 10);
+// }
+
+// // document.getElementById("target").addEventListener("click", fadeOutEffect);
