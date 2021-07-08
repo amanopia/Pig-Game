7afcecd0ce6658b1bbdd16c0d16062699f82712b
Aman <fz72eaman@gmail.com>
2021-07-08 23:45:07 +0530

    add player switching functionality

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const hold1 = document.querySelector(".btn--hold--1");
 const roll1 = document.querySelector(".btn--roll--1");
 const hold2 = document.querySelector(".btn--hold--2");
 const roll2 = document.querySelector(".btn--roll--2");
+// DICE IMAGE
 const dice1 = document.getElementById("dice--1");
 const dice2 = document.getElementById("dice--2");
 
@@ -16,11 +17,14 @@ let playing = true;
 score1.textContent = 0;
 score2.textContent = 0;
 
+let scores = [0, 0];
+let activePlayer = 0;
+
 let currentScore1 = 0;
 let currentScore2 = 0;
 
 roll1.addEventListener("click", function () {
-  if (playing) {
+  if (playing && activePlayer === 0) {
     let ranBtnRoll = Math.ceil(Math.random() * 6);
     dice1.src = `dice-${ranBtnRoll}.png`;
 
@@ -29,15 +33,28 @@ roll1.addEventListener("click", function () {
       score1.textContent = currentScore1;
     } else {
       dice1.classList.add("hidden");
+      dice2.classList.remove("hidden"); // removing the hidden class here immediately makes the dice visible, as compared to the button being clicked by the other user and then the dice becoming visible.
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      console.log(activePlayer);
     }
   }
 
   //   console.log(ranBtnRoll);
 });
 roll2.addEventListener("click", function () {
-  if (playing) {
+  if (playing && activePlayer === 1) {
     let ranBtnRoll = Math.ceil(Math.random() * 6);
     dice2.src = `dice-${ranBtnRoll}.png`;
+
+    if (ranBtnRoll !== 1) {
+      currentScore2 += ranBtnRoll;
+      score2.textContent = currentScore2;
+    } else {
+      dice2.classList.add("hidden");
+      dice1.classList.remove("hidden"); // removing the hidden class here immediately makes the dice visible, as compared to the button being clicked by the other user and then the dice becoming visible.
+      activePlayer = activePlayer === 1 ? 0 : 1;
+      console.log(activePlayer);
+    }
   }
 });
 // // const body = document.body;
